f2f38ccda575baac9e3c135a7d57132d5a0697bb
dinaramairambaeva <78029791+dinaramairambaeva@users.noreply.github.com>
2022-05-20 11:29:35 +0600

    Delete src directory

--- src/main/java/com/example/finalproject/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.example.finalproject
-
-class Constants {
-    companion object{
-        public const val API = "https://api.covid19api.com/countries"
-    }
-}
--- src/main/java/com/example/finalproject/models/CountriesRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.finalproject.models
-
-import retrofit2.Call
-
-class CountriesRepository() {
-    fun getAllCountries() : Call<List<Countries>> = ApiInterface.create().getCountries()
-}
